6caba81b2320a9909f48b6294fd57a1904120e6b
buhleqampi <buhle3857@gmail.com>
2024-10-09 17:57:05 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+//      Strings
+        String name = "Mike";
+        name = "Bob";
+        System.out.println(name);
+
+//      int
+        int number;
+        number = 25;
+        System.out.println(number);
+
+//      Double
+        double unit;
+        unit = 45.67;
+        System.out.println(unit);
+
+        double x ;
+        x = 34;
+        double y = 12;
+        System.out.println(x/y);
+
+//      Float
+        float measure;
+        measure = 45.67f;
+        System.out.println(measure);
+
+//      Characters
+        char letters;
+        letters = 'B';
+        System.out.println(letters);
+
+//      Boolean
+        boolean isOpen = true;
+        boolean isClosed = false;
+        System.out.println(isOpen);
+        System.out.println(isClosed);
+
+//      Math
+        int z = 3;
+        int w = 2;
+        System.out.println(z+w);
+        System.out.println(z*w);
+        System.out.println(z/w);
+        System.out.println(z-w);
+
+    }
+}
